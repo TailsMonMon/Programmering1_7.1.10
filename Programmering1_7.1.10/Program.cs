7c9aec0059e9ac3f0d15d23edc7038686689a8eb
Monica Björk <m.bjork95@gmail.com>
2018-04-04 14:27:54 +0200

    Add project files.

---
+++ Programmering1_7.1.10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_7._1._10 {
+    class Program {
+        static void Main(string[] args) {
+            int a = 8%3;
+            Console.WriteLine("a "+a);   // Output: 2
+
+            int b = 9%6;
+            Console.WriteLine("b "+b);   // Output: 3
+
+            int c = 9%5;
+            Console.WriteLine("c "+c);   // Output: 4
+
+            int d = 10%5;
+            Console.WriteLine("d "+d);   // Output: 0
+
+            int e = 28&10;
+            Console.WriteLine("e "+e);   // Output: 8
+
+            Console.ReadLine();
+        }
+    }
+}
